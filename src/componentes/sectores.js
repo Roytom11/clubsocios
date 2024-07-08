ab3f529c06eaf074b2ce8f3d45ad4d9093c1aca2
Roytom11 <rodrigobrzek11@gmail.com>
2024-07-08 12:53:54 -0300

    first commit

---
+++ src/componentes/sectores.js
@@ -0,0 +1,8 @@
+const sectores = [
+    { "id" : 1, "sector": "Popular norte baja", "precio": 0 },
+    { "id" : 2, "sector": "Platea norte alta", "precio": 500},
+    { "id" : 3, "sector": "Erico alta", "precio": 1000},
+    { "id" : 4, "sector": "Bochini", "precio": 1500}
+]
+
+export default sectores
